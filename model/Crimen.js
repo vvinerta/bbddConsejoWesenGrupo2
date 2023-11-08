e96578c9286c1585377edaed55c2f10afd81827e
diurvi <diegourmente@gmail.com>
2023-11-08 17:02:15 +0100

    Clases de crimen y de wesen que forman el modelo del proyecto

---
+++ model/Crimen.js
@@ -0,0 +1,48 @@
+class Crimen {
+    constructor(denominacion, fechaRealizacion, involucradosKehrseiter, victimas, descripcion) {
+        this.denominacion = denominacion;
+        this.fechaRealizacion = fechaRealizacion;
+        this.involucradosKehrseiter = involucradosKehrseiter;
+        this.victimas = victimas;
+        this.descripcion = descripcion;
+    }
+
+    getDenominacion(){
+        return this.denominacion;
+    }
+
+    setDenominacion(nuevaDenominacion){
+        this.denominacion = nuevaDenominacion;
+    }
+
+    getFechaRealizacion(){
+        return this.fechaRealizacion;
+    }
+
+    setFechaRealizacion(nuevaFechaRealizacion){
+        this.fechaRealizacion = nuevaFechaRealizacion;
+    }
+
+    getInvolucradosKehrseiter(){
+        return this.involucradosKehrseiter;
+    }
+
+    setInvolucradosKehrseiter(nuevaInvolucradosKehrseiter){
+        this.involucradosKehrseiter = nuevaInvolucradosKehrseiter;
+    }
+
+    getVictimas(){
+        return this.victimas;
+    }
+
+    setVictimas(nuevaVictima){
+        this.victimas = nuevaVictima;
+    }
+    
+    denominacion = "";
+    fechaRealizacion = "";
+    involucradosKehrseiter = "";
+    victimas = 0;
+    descripcion = "";
+}
+
---
+++ model/Wesen.js
@@ -0,0 +1,127 @@
+class Wesen {  
+    constructor(nombreApellidos, direccion, nacionalidad, tipo, crimenes, foto, 
+        apercibido, gravedadFaltas, wesenComplices, sicario, castigo, fechaCondena) {
+        this.nombreApellidos = nombreApellidos;
+        this.direccion = direccion;
+        this.nacionalidad = nacionalidad;
+        this.tipo = tipo;
+        this.crimenes = crimenes;
+        this.foto = foto;
+        this.apercibido = apercibido;
+        this.gravedadFaltas = gravedadFaltas;
+        this.wesenComplices = wesenComplices;
+        this.sicario = sicario;
+        this.castigo = castigo;
+        this.fechaCondena = fechaCondena;
+    }
+
+    getNombre() {
+        return this.nombreApellidos;
+    }
+
+    setNombre(nuevoNombre) {
+        this.nombreApellidos = nuevoNombre;
+    }
+    getDireccion() {
+        return this.direccion;
+    }
+
+    setDireccion(nuevaDireccion) {
+        this.direccion = nuevaDireccion;
+    }
+
+    getNacionalidad() {
+        return this.nacionalidad;
+    }
+
+    setNacionalidad(nuevaNacionalidad) {
+        this.nacionalidad = nuevaNacionalidad;
+    }
+
+    getTipo() {
+        return this.tipo;
+    }
+
+    setTipo(nuevoTipo) {
+        this.tipo = nuevoTipo;
+    }
+
+    getCrimenes() {
+        return this.crimenes;
+    }
+
+    setCrimenes(nuevosCrimenes) {
+        this.crimenes = nuevosCrimenes;
+    }
+
+    getFoto() {
+        return this.foto;
+    }
+
+    setFoto(nuevaFoto) {
+        this.foto = nuevaFoto;
+    }
+
+    getApercibido() {
+        return this.apercibido;
+    }
+
+    setApercibido(nuevoApercibido) {
+        this.apercibido = nuevoApercibido;
+    }
+
+    getGravedadFaltas() {
+        return this.gravedadFaltas;
+    }
+
+    setGravedadFaltas(nuevaGravedadFaltas) {
+        this.gravedadFaltas = nuevaGravedadFaltas;
+    }
+
+    getWesenComplices() {
+        return this.wesenComplices;
+    }
+
+    setWesenComplices(nuevosWesenComplices) {
+        this.wesenComplices = nuevosWesenComplices;
+    }
+
+    getSicario() {
+        return this.sicario;
+    }
+
+    setSicario(nuevoSicario) {
+        this.sicario = nuevoSicario;
+    }
+
+    getCastigo() {
+        return this.castigo;
+    }
+
+    setCastigo(nuevoCastigo) {
+        this.castigo = nuevoCastigo;
+    }
+
+    getFechaCondena() {
+        return this.fechaCondena;
+    }
+
+    setFechaCondena(nuevaFechaCondena) {
+        this.fechaCondena = nuevaFechaCondena;
+    }
+
+    nombreApellidos = "";
+    direccion = "";
+    nacionalidad = "";
+    tipo = "";
+    crimenes = ["", "", ""];
+    foto = "";
+    apercibido = true; 
+    gravedadFaltas = 5; 
+    wesenComplices = ["", ""];
+    sicario = "";
+    castigo = ["", ""];
+    fechaCondena = "";
+}
+
+   
