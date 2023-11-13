f135689794b614efe4d054e263c4e5ad5c429762
Guillermo <guillermofabrod@gmial.com>
2023-11-13 19:56:04 +0100

    Lista Crimenes y Mejora de Lista Wesen

--- controller/listadoCrimenController.js
+++ controller/listadoCrimenController.js
@@ -1,16 +1,39 @@
 var lista =document.getElementById("listaC");  
 
 let wesendao=new CrimenDao(); 
-let arraycrimen=wesendao.getAllCrimenes();
+// let b = new Crimen("Allanamiento","10-2-1998",["Carlos Manuel", "Francisco Javier"],["Luis Miguel"],"Han entrado en la casa de Luis Miguel");
+// let a = new Crimen("Allanamiento","10-2-1998",["Carlos Manuel", "Francisco Javier"],["Luis Miguel"],"Han entrado en la casa de Luis Miguel");
+let arraycrimen=[];
+arraycrimen=wesendao.getAllCrimenes();
+// arraycrimen.push(a);
+// arraycrimen.push(b);
 
 arraycrimen.forEach(element => {
 
+let involucrados=listararrays(element.getInvolucradosKehrseiter());
+let victimas=listararrays(element.getVictimas());
+
+
 var li=document.createElement("li");
-li.innerHTML=element.getDenominacion()+"<br>"+
-element.getFechaRealizacion()+"<br>"+
-element.getInvolucradosKehrseiter()+"<br>"+
-element.getVictimas()+"<br>"+
-element.getDescripcion()+"<br><br>";
+li.innerHTML="<div class='horizontal'>"+
+"<div class='info'>"+
+"Denominacion : "+ element.getDenominacion()+"<br>"+
+"Fecha : "+element.getFechaRealizacion()+"<br>"+
+"-------------------------------------------------<br>"+
+"<b>Involucrado/s :</b><br>"+involucrados+"<br>"+
+"<b>Victima/s : </b><br>"+victimas+"</div>"+
+"<div class='suelo'>"+
+"Descripción : <br><textarea cols='50' rows='5'>"+element.getDescripcion()+"</textarea>"+"</div></div>"
+
 
 lista.appendChild(li);
 });
+
+function listararrays(array){
+   
+    let involucrados="";
+    array.forEach(invo => {
+        involucrados = involucrados.concat(invo, "<br>");
+    });
+return involucrados;
+}
--- controller/listadoWesenController.js
+++ controller/listadoWesenController.js
@@ -1,17 +1,19 @@
 
 var lista =document.getElementById("listaW");  
-let wesen1 = new Wesen("Anyah Lebroska", "C/ Verguno Tercero", "Rusa", "Perro", ["Trafico de Organos"], "https://img.freepik.com/foto-gratis/vista-frontal-retrato-belleza-joven-hembra-rostro_186202-460.jpg?size=626&ext=jpg&ga=GA1.1.1826414947.1699833600&semt=sph", true, 4, ["Cómplice1", "Cómplice2"], "Don Manuel", "Empalacion", "12-12-2010");
-let wesen2 = new Wesen("Nombre Apellidos2", "Dirección", "Nacionalidad", "Tipo", 5, "foto.jpg", true, "Gravedad", ["Cómplice1", "Cómplice2"], true, "Castigo", "2023-11-08");
+// let wesen1 = new Wesen("Anyah Lebroska", "C/ Verguno Tercero", "Rusa", "Perro", ["Trafico de Organos"], "https://img.freepik.com/foto-gratis/vista-frontal-retrato-belleza-joven-hembra-rostro_186202-460.jpg?size=626&ext=jpg&ga=GA1.1.1826414947.1699833600&semt=sph", true, 4, ["Antonio Vargas Heredia", "Francisco Alegre"], "Don Manuel", "Empalacion", "12-12-2010");
+// let wesen2 = new Wesen("Anyah Lebroska", "C/ Verguno Tercero", "Rusa", "Perro", ["Trafico de Organos"], "https://img.freepik.com/foto-gratis/vista-frontal-retrato-belleza-joven-hembra-rostro_186202-460.jpg?size=626&ext=jpg&ga=GA1.1.1826414947.1699833600&semt=sph", true, 4, ["Antonio Vargas Heredia", "Francisco Alegre"], "Don Manuel", "Empalacion", "12-12-2010");
 
 let wesendao=new WesenDao(); 
-let arraywesens=wesendao.getAllWesens();
+let arraywesens=[];
+ arraywesens=wesendao.getAllWesens();
 
-//USAR PAR PRUEBA
-arraywesens=[wesen1,wesen2];
+// arraywesens=[wesen1,wesen2];
 
 arraywesens.forEach(element => {
 
     formateardatos(element);
+    let complices=listararrays(element.getWesenComplices())
+
 
 var li=document.createElement("li");
 li.innerHTML=
@@ -22,13 +24,14 @@ li.innerHTML=
 "Nacionalidad : "+element.getNacionalidad()+"<br>"+
 "Direccion : "+element.getDireccion()+"</div>"+
 "<div name='datofoto'><img src="+element.getFoto()+" alt='Foto del criminal'></div></div>"+
-"<div class='horizontal'>------------------------------------------------------------------------------</div>"+
+"<div class='horizontal2'>------------------------------------------------------------------------------</div>"+
 "<div name='datoscrimen' class='horizontal'>"+
 "<div name='infocrimen' class='info suelo'>"+
 "Crimen/es : "+element.getCrimenes()+"<br>"+
 "Gravedad : "+element.getGravedadFaltas()+"<br>"+
 "Apercibido : "+element.getApercibido()+"<br>"+
-"Complices : "+element.getWesenComplices()+"</div>"+
+"<b>Complices :</b><br>"+complices
++"</div>"+
 "<div name='infoley'>"+
 "Castigo : "+element.getCastigo()+"<br>"+
 "Sicario : "+element.getSicario()+"<br>"+
@@ -44,4 +47,11 @@ if(objeto.getApercibido()){
     objeto.setApercibido("No");
 }
 
+}
+function listararrays(array){
+    let involucrados="";
+    array.forEach(invo => {
+        involucrados = involucrados.concat(invo, "<br>");
+    });
+return involucrados;
 }
