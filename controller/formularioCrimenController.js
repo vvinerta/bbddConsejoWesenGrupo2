dba9b19d1f2f66b81b31bd80d721a3a596bf4253
vvinerta <vinertacrespo@gmail.com>
2023-11-14 20:16:30 +0100

    Funcionalidades de formularioCrimen

---
+++ controller/formularioCrimenController.js
@@ -0,0 +1,29 @@
+
+let txtDenominacion = document.getElementById("denominacion");
+let txtFechaRealizacion = document.getElementById("fechaRealizacion");
+let txtInvolucradosKehrseiter = document.getElementById("involucradosKehrseiter");
+let txtVictimas = document.getElementById("victimas");
+let txtDescripcion = document.getElementById("descripcion");
+
+function guardarCrimen(){
+
+    if(!document.getElementById("crimenForm").checkValidity()){
+        return;
+    }
+
+    let denominacion = txtDenominacion.value;
+    let fechaRealizacion = txtFechaRealizacion.value;
+    let involucradosKehrseiter = txtInvolucradosKehrseiter.value;
+    let victimas = txtVictimas.value;
+    let descripcion = txtDescripcion.value;
+
+    let miCrimen = new Crimen(denominacion, fechaRealizacion, involucradosKehrseiter, victimas, descripcion);
+
+    let crimenDao = new CrimenDao();
+
+    crimenDao.addCrimen(miCrimen);
+
+    alert("Crimen añadido correctamente a la base de datos");
+
+
+}
