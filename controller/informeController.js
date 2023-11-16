eefd7dd5db77cbda657fa7ac7f8e5fe25f614bed
salejaldre <sergioalejaldrezarza@gmail.com>
2023-11-16 19:06:11 +0100

    Mejoras informe
    Cambio de los estilos y en la forma de aplicar los filtros

--- controller/informeController.js
+++ controller/informeController.js
@@ -1,5 +1,9 @@
 var service = new WesenService();
 
+/**
+ * Método para obtener la lista de datos del localStorage, ordenarla por la fecha de la condena,
+ * el número de crimenes y la gravedad de las faltas y mostrarla en formato lista
+*/
 function cargarListaYmostrar() {
   var listaWesens = service.getAllWesens();
 
@@ -16,17 +20,18 @@ function cargarListaYmostrar() {
   listaWesens.forEach((wesen) => {
     var li = document.createElement("li");
     li.innerHTML =
-      wesen.getNombre() +
       "<br>" +
-      wesen.getNacionalidad() +
+      "Nombre: "+wesen.getNombre() +
       "<br>" +
-      wesen.getTipo() +
+      "Nacionalidad: "+wesen.getNacionalidad() +
       "<br>" +
-      wesen.getFechaCondena() +
+      "Tipo: "+wesen.getTipo() +
       "<br>" +
-      wesen.getGravedadFaltas() +
+      "Fecha Condena: "+wesen.getFechaCondena() +
       "<br>" +
-      wesen.getCrimenes() +
+      "Gravedad Faltas: "+wesen.getGravedadFaltas() +
+      "<br>" +
+      "Crimenes: "+wesen.getCrimenes() +
       "<br>" +
       "<br>";
 
@@ -34,6 +39,10 @@ function cargarListaYmostrar() {
   });
 }
 
+/**
+ * @param {Array<Wesen>} listaWesens lista de wesens a ordenar
+ * @returns Array<Wesen> lista con los datos ordenados
+ */
 function ordenar(listaWesens) {
   listaWesens = service.sortByFechaCondena(listaWesens);
   listaWesens = service.sortByNumeroCrimenes(listaWesens);
@@ -42,72 +51,104 @@ function ordenar(listaWesens) {
   return listaWesens;
 }
 
+// Recoger los inputs relacionados con el filtro
+var selectFiltro = document.getElementById('filtro');
+var inputValorFiltro = document.getElementById('valorFiltro');
+
+// Recoger la lista
+var listaInforme = document.getElementById('informe');
+
+/**
+ * Añadir un evento al campo desplegable, para que cuando el usuario seleccione una opcion
+ * se resetee tanto el dato del campo de texto como el filtro aplicado previamente y se
+ * muestre toda la lista de daos que hay en el localStorage
+*/ 
+selectFiltro.addEventListener('change', function(){
+  inputValorFiltro.value = "";
+  cargarListaYmostrar();
+});
+
+/**
+ * Añadir un evento de entrada al campo de texto 'inputValorFiltro' que llamará a la función
+ * 'aplicarFiltro' cada vez que se introduzca un carácter en el campo
+*/
+inputValorFiltro.addEventListener('input', aplicarFiltro);
+
+/**
+ * Función que aplica un filtro dinámico a una lista de Wesens según el valor seleccionado 
+ * y el texto ingresado
+*/
 function aplicarFiltro() {
+  // Obtengo la lista completa de Wesens
   var listaDeWesensens = service.getAllWesens();
 
-  var filtro = document.getElementById("filtro").value;
-  var valorFiltro = document.getElementById("valorFiltro").value;
+  // Obtengo el valor seleccionado del filtro y el texto ingresado en el campo
+  var filtro = selectFiltro.value;
+  var valorFiltro = inputValorFiltro.value;
 
-  if (filtro == "" || valorFiltro.trim() == "") {
+   // Verifica si no se ha seleccionado un filtro, muestra una alerta y se vacia el campo de texto
+  if(filtro === "") {
     alert("Debe rellenar los campos");
-  } else {
-    for (var i = 0; i < listaDeWesensens.length; i++) {
+    inputValorFiltro.value = "";
+    return;
+  }
+
+  // Verifica si el texto ingresado está vacío y carga la lista completa
+  if(valorFiltro.trim() === ""){
+    listaDeWesensens = service.getAllWesens();
+  } else{
+  // Filtra dinámicamente la lista según el filtro seleccionado y el texto ingresado
+  for (var i = 0; i < listaDeWesensens.length; i++) {
       switch (filtro) {
-        case "nombre":
-          listaDeWesensens = service.getWesensByName(valorFiltro);
-          break;
-        case "pais":
-          listaDeWesensens = service.getWesensByPais(valorFiltro);
-          break;
-        case "tipo":
-          listaDeWesensens = service.getWesensByType(valorFiltro);
-          break;
-        case "crimen":
-          listaDeWesensens = service.getWesensByCrimen(valorFiltro);
-          break;
-        case "gravedad":
-          listaDeWesensens = service.getWesensByGravedadMayorA(valorFiltro);
-          break;
+          case "nombre":
+            listaDeWesensens = service.getWesensByName(valorFiltro);
+            break;
+          case "pais":
+            listaDeWesensens = service.getWesensByPais(valorFiltro);
+            break;
+          case "tipo":
+            listaDeWesensens = service.getWesensByType(valorFiltro);
+            break;
+          case "crimen":
+            listaDeWesensens = service.getWesensByCrimen(valorFiltro);
+            break;
+          case "gravedad":
+            listaDeWesensens = service.getWesensByGravedad(valorFiltro);
+            break;
+          default:
+            return false;
       }
-
-      var lista = document.getElementById("informe");
-
-      while (lista.firstChild) {
-        lista.removeChild(lista.firstChild);
-      }
-
-      listaDeWesensens.forEach((wesen) => {
-        var li = document.createElement("li");
-        li.innerHTML =
-          wesen.getNombre() +
-          "<br>" +
-          wesen.getNacionalidad() +
-          "<br>" +
-          wesen.getTipo() +
-          "<br>" +
-          wesen.getFechaCondena() +
-          "<br>" +
-          wesen.getGravedadFaltas() +
-          "<br>" +
-          wesen.getCrimenes() +
-          "<br>" +
-          "<br>";
-
-        lista.appendChild(li);
-      });
     }
   }
+
+  // Limpia la lista actual
+  while (listaInforme.firstChild) {
+      listaInforme.removeChild(listaInforme.firstChild);
+  }
+
+  // Agrega los elementos filtrados a la lista
+  listaDeWesensens.forEach(function (wesen) {
+      var li = document.createElement("li");
+      li.innerHTML =
+        "<br>" +
+        "Nombre: " + wesen.getNombre() +
+        "<br>" +
+        "Nacionalidad: " + wesen.getNacionalidad() +
+        "<br>" +
+        "Tipo: " + wesen.getTipo() +
+        "<br>" +
+        "Fecha Condena: " + wesen.getFechaCondena() +
+        "<br>" +
+        "Gravedad Faltas: " + wesen.getGravedadFaltas() +
+        "<br>" +
+        "Crimenes: " + wesen.getCrimenes() +
+        "<br>" +
+        "<br>";
+      listaInforme.appendChild(li);
+  });
 }
 
-function borrarFiltro() {
-  var filtro = document.getElementById("filtro");
-  var valorFiltro = document.getElementById("valorFiltro");
-
-  filtro.value = "";
-  valorFiltro.value = "";
-  cargarListaYmostrar();
-}
-
+// Función para descargar la lista de Wesens como un archivo PDF y luego restaurar el contenido original y volver a mostrar la lista de todos los Wesens
 function descargarLista() {
   var printContents = document.getElementById("informe").innerHTML;
   var originalContents = document.body.innerHTML;
@@ -117,6 +158,8 @@ function descargarLista() {
   window.print();
 
   document.body.innerHTML = originalContents;
+  cargarListaYmostrar();
+  location.reload(true);
 }
 
 cargarListaYmostrar();
